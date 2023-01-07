9725e349466114e3dd737966c24f3b1cd40ebb10
samimeadad <sami.meadad@gmail.com>
2023-01-07 13:01:55 +0600

    Home review carousal modified

--- src/Pages/Home/HomeCarousel.js
+++ src/Pages/Home/HomeCarousel.js
@@ -1,9 +1,5 @@
 import React, { useState } from 'react';
 import { Carousel } from 'react-bootstrap';
-import carousel1 from '../../images/home/image-slider/carousel1.jpg';
-import carousel2 from '../../images/home/image-slider/carousel2.jpg';
-import carousel3 from '../../images/home/image-slider/carousel3.jpg';
-import carousel4 from '../../images/home/image-slider/carousel4.jpg';
 import carousel5 from '../../images/home/image-slider/carousel5.jpg';
 
 const HomeCarousel = () => {
@@ -15,42 +11,6 @@ const HomeCarousel = () => {
 
     return (
         <Carousel activeIndex={ index } onSelect={ handleSelect } className="text-center overflow-hidden">
-            <Carousel.Item>
-                <img
-                    className="d-block img-fluid"
-                    src={ carousel1 }
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block img-fluid"
-                    src={ carousel2 }
-                    alt="Second slide"
-                />
-                <Carousel.Caption>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block img-fluid"
-                    src={ carousel3 }
-                    alt="Third slide"
-                />
-                <Carousel.Caption className='text-dark'>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block img-fluid"
-                    src={ carousel4 }
-                    alt="Fourth slide"
-                />
-                <Carousel.Caption className='text-dark'>
-                </Carousel.Caption>
-            </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block img-fluid"
