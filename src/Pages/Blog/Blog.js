3a16d00279fad79b182f52dbd6d10165ba25efa9
samimeadad <sami.meadad@gmail.com>
2023-08-11 14:07:46 +0600

    all blog done

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -18,13 +18,13 @@ const Blog = () => {
                         <Card style={ { width: '18rem', height: "100%" } }>
                             <Card.Img variant="top" src={ blog1 } className="img-fluid m-auto" />
                             <Card.Body>
-                                <Card.Title className='fs-5'>Scottish tax bands diverge further</Card.Title>
+                                <Card.Title className='fs-5'><Link to="/blog/blog-1" className='text-decoration-none text-dark'>Scottish tax bands diverge further</Link></Card.Title>
                                 <Card.Text>
                                     <p>
                                         On the 12 December 2018, Derek Mackay, the Scottish Finance Secretary, unveiled the Scottish budget for 2019/20. You will recall that this time last year he introduced two additional tax bands, which meant that Scottish resident tax payers...
                                     </p>
                                 </Card.Text>
-                                <Link to="/blog/blog-1"><Button variant="dark">Read More</Button></Link>
+                                <Link to="/blog/blog-1" className='text-decoration-none text-warning'><h6>Read More...</h6></Link>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -32,13 +32,13 @@ const Blog = () => {
                         <Card style={ { width: '18rem', height: "100%" } }>
                             <Card.Img variant="top" src={ blog2 } className="img-fluid m-auto" />
                             <Card.Body>
-                                <Card.Title className='fs-5'>Advisory fuel rates for company cars</Card.Title>
+                                <Card.Title className='fs-5'><Link to="/blog/blog-1" className='text-decoration-none text-dark'>Advisory fuel rates for company cars</Link></Card.Title>
                                 <Card.Text>
                                     <p>
                                         From 1st December 2018, there have been new company car advisory fuel rates published, stating that the previous rates can be used for up to one month from the date the new rates apply. The rates only applied to employees using a company car, and the...
                                     </p>
                                 </Card.Text>
-                                <Link to="/blog/blog-2"><Button variant="dark">Read More</Button></Link>
+                                <Link to="/blog/blog-2" className='text-decoration-none text-warning'><h6>Read More...</h6></Link>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -46,19 +46,19 @@ const Blog = () => {
                         <Card style={ { width: '18rem', height: "100%" } }>
                             <Card.Img variant="top" src={ blog3 } className="img-fluid m-auto" />
                             <Card.Body>
-                                <Card.Title className='fs-5'>Marketing considerations for 2019</Card.Title>
+                                <Card.Title className='fs-5'><Link to="/blog/blog-1" className='text-decoration-none text-dark'>Marketing considerations for 2019</Link></Card.Title>
                                 <Card.Text>
                                     <p>
                                         As we leave 2018 behind, it's worth taking stock of this period of increased regulation on customer data protection and privacy - in parallel with its acceleration in marketing technology capabilities. This blog provides an overview of what to consider in terms of the...
                                     </p>
                                 </Card.Text>
-                                <Link to="/blog/blog-3"><Button variant="dark">Read More</Button></Link>
+                                <Link to="/blog/blog-3" className='text-decoration-none text-warning'><h6>Read More...</h6></Link>
                             </Card.Body>
                         </Card>
                     </Col>
                 </Row>
             </div>
-        </Container>
+        </Container >
     );
 };
 
--- src/Pages/Blog/Blog1.js
+++ src/Pages/Blog/Blog1.js
@@ -9,8 +9,8 @@ const Blog1 = () => {
             <section className='my-5'>
                 <h1>Scottish tax bands diverge further</h1>
                 <h6 className='text-secondary my-3'>by Admin | Jan 28, 2019 | Tax</h6>
-                <p><Link to="/blog" className='text-dark text-decoration-none'>Don't wanna read? Go Back!!!</Link></p>
-                <h1 className='fs-1 text-warning fw-bold'>__________________________________________________________</h1>
+                <p><Link to="/blog" className='text-warning text-decoration-none'>Don't wanna read? Go Back!!!</Link></p>
+                <h1 className='fs-1 text-warning fw-bold'>___________________________________________________________</h1>
             </section>
             <section>
                 <img src={ blogImage } alt="BannerImage" className='img-fluid w-75 overflow-hidden' />
@@ -28,9 +28,102 @@ const Blog1 = () => {
                 <p className='text-justify my-4'>
                     This year, it's okay - you can breathe a sigh of relief. Mackay has resisted the temptation to introduce further divergence in the rate of income tax this year.
                 </p>
-                <h3>Same Allowances</h3>
+                <h3 className='text-justify mt-5 mb-3'>Same Allowances</h3>
                 <p className='text-justify my-4'>
                     For 2019/20, a Scottish taxpayer will be entitled to the same &pound;12,500 personal allowance as the rest of the UK. The Scottish allowance will also be withdrawn at the same rate of &pound;1 for every &pound;2 of adjusted net income over &pound;100,000.
+                </p>
+                <p className='text-justify my-5'>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Band in 2019/20</th>
+                                <th scope="col">Name of Band</th>
+                                <th scope="col">Income tax rate %</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>12,501 - 14,549</td>
+                                <td>Starter rate</td>
+                                <td>19</td>
+                            </tr>
+                            <tr>
+                                <td>14,550 - 24,944</td>
+                                <td>Basic rate</td>
+                                <td>20</td>
+                            </tr>
+                            <tr>
+                                <td>24,945 - 43,430</td>
+                                <td>Intermediate rate</td>
+                                <td>21</td>
+                            </tr>
+                            <tr>
+                                <td>43,431 - 150,000</td>
+                                <td>Higher rate</td>
+                                <td>41</td>
+                            </tr>
+                            <tr>
+                                <td>&gt;150,000</td>
+                                <td>Top rate</td>
+                                <td>46</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </p>
+                <h3 className='text-justify mt-5 mb-3'>National Insurance</h3>
+                <p className='text-justify my-4'>
+                    Due to the fact that the power to set NIC rates and thresholds has not been devolved, the rates and thresholds applicable in Scotland are the same as in the rest of the UK.
+                </p>
+                <p className='text-justify my-4'>
+                    In her recent AccountingWEB article, Rebecca Cave drew attention to the fact that "Combining the NIC and income tax rates for a Scottish taxpayer produces some very odd marginal rates."
+                </p>
+                <h3 className='text-justify mt-5 mb-3'>Land and Building Transaction Tax changes</h3>
+                <p className='text-justify my-4'>
+                    Mackay is to increase Scotland's Land and Building Transaction Tax (LBTT) and Additional Dwelling Supplement from 3% to 4%, effective from the 25 January 2019. This will take it out of line with the English equivalent Stamp Duty Land and Tax (SDLT) ADS rate of 3%.
+                </p>
+                <p className='text-justify my-4'>
+                    LBTT changes do not stop here. On the same date (25 January), the LBTT rates applicable to commercial properly are also set to change:
+                </p>
+                <p className='text-justify'>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Threshold</th>
+                                <th scope="col">Percentage to 25/01/2019</th>
+                                <th scope="col">Percentage from 25/01/2019</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Up to 150,000</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>150,001 - 250,000</td>
+                                <td>3</td>
+                                <td>1</td>
+                            </tr>
+                            <tr>
+                                <td>250,001 - 350,000</td>
+                                <td>3</td>
+                                <td>5</td>
+                            </tr>
+                            <tr>
+                                <td>&gt;350,001</td>
+                                <td>4.5</td>
+                                <td>5</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </p>
+                <h3 className='text-justify mt-5 mb-3'>The shape of things to come</h3>
+                <p className='text-justify my-4'>
+                    Unless the Chancellor's threat of a Spring Budget comes to fruition, we will have to wait until next autumn to see if the changes to LBTT will be mirrored by the similar SDLT changes in England and Northern Ireland.
+                </p>
+                <h3 className='text-justify mt-5 mb-3'>What about Wales?</h3>
+                <p className='text-justify my-4'>
+                    Many of you reading this article will recall that since the 1 April 2018, the Welsh Assembly replaced SDLT with Land Transaction Tax (LTT). It is, therefore, entirely possible that Wales might choose to adopt similar rates to those proposed by Scotland, or even to do something entirely different.
                 </p>
             </section>
             <p className="card-text"><Link to="/blog"><Button variant="dark">Go Back</Button></Link></p>
